bad00c55b44a51c89a55584db1226703c5b4203e
elmvel <goodd7183+2@gmail.com>
2025-11-24 11:25:13 -0500

    Fix run.sh to be more usable

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+set -e
+
 binary="fc"
 run=0
 
 if ! [ -z "$1" ]
 then
     binary="$1"
+    shift
     run=1
 fi
 
 cmake --build build/
 if [ "$run" -eq 1 ]
 then
-    ./build/$binary
+    ./build/"$binary" "$@"
 fi
